7492fe481cccd908107389d10507037a55097fa0
teraaret <raiiliiat@yandex.ru>
2017-10-24 22:09:21 +0500

    Adds sha256 and change input's type to password

---
+++ README.md
@@ -0,0 +1,5 @@
+# php-version on ar site
+
+24.10.2017
+
+- Adds sha256 password;
--- auth.php
+++ auth.php
@@ -1,18 +1,13 @@
 <?php
 
-$users = ['caesar'];
+$hashes = ['203b8ade4aaee21728c5ecc2b53aadbe0cd25c7337c205da91f0686542ad3478'];
 
-for ($i = 0; $i < count($users) ; $i++) {
-    if ( $_POST['login'] == $users[$i] )
+for ($i = 0; $i < count($hashes); $i++) {
+    if ( hash("sha256", $_POST['login']) == $hashes[$i] )
     {
         setcookie("session","admin",time()+3600);
     }
 }
-
-//if ( $_POST['login'] == 'admin' )
-//{
-//    setcookie("session","admin",time()+3600);
-//}
 
 if ( $_GET['action'] == 'logout' )
 {
